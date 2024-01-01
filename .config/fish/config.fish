5cf7b6d061b7aa542b47cbb5c7735400e6b86aa1
Andrei Zhigalkin <snacks64@proton.me>
2024-01-01 18:56:54 +0600

    Initial commit

---
+++ .config/fish/config.fish
@@ -0,0 +1,15 @@
+if status is-interactive
+    set fish_greeting
+
+    set -x XDG_CACHE_HOME $HOME/.cache
+    set -x XDG_CONFIG_HOME $HOME/.config
+    set -x XDG_DATA_HOME $HOME/.local/share
+    set -x XDG_STATE_HOME $HOME/.local/state
+
+    set -x DOCKER_CONFIG $XDG_CONFIG_HOME/docker
+
+    fish_add_path $HOME/.dotfiles/.local/bin
+    fish_add_path $HOME/.local/bin
+
+    set -x GPG_TTY (tty)
+end

d503c9d739def39486a24fe179cc4d38f2bc068e
Andrei Zhigalkin <snacks64@proton.me>
2025-03-07 02:38:07 +0500

    Update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,5 +10,6 @@ if status is-interactive
     set -x XDG_DATA_HOME $HOME/.local/share
     set -x XDG_STATE_HOME $HOME/.local/state
 
+    set -x COMPOSE_BAKE true
     set -x DOCKER_CONFIG $XDG_CONFIG_HOME/docker
 end

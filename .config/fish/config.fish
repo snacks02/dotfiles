c55139b0d403a0e39becc20919d09d49a781328b
Andrei Zhigalkin <snacks64@proton.me>
2025-09-10 19:51:08 +0500

    Update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,6 +10,7 @@ if status is-interactive
     set -x XDG_DATA_HOME $HOME/.local/share
     set -x XDG_STATE_HOME $HOME/.local/state
 
+    set -x CLAUDE_CONFIG_DIR $XDG_DATA_HOME/claude
     set -x COMPOSE_BAKE true
     set -x DOCKER_CONFIG $XDG_CONFIG_HOME/docker
 end

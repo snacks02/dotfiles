dc2c4c2a08dc7c8a8f3bb61ee5f42a9a343b2d3c
Andrei Zhigalkin <snacks64@proton.me>
2025-11-28 13:29:24 +0500

    Update

--- configuration/fish/config.fish
+++ configuration/fish/config.fish
@@ -2,16 +2,6 @@ if status is-interactive
     set fish_greeting
 
     fish_add_path "$HOME/.cargo/bin"
-    fish_add_path "$HOME/.dotfiles/scripts/path"
+    fish_add_path "$HOME/.dotfiles/scripts"
     fish_add_path "$HOME/.local/bin"
-
-    set --export XDG_CACHE_HOME "$HOME/.cache"
-    set --export XDG_CONFIG_HOME "$HOME/.config"
-    set --export XDG_DATA_HOME "$HOME/.local/share"
-    set --export XDG_STATE_HOME "$HOME/.local/state"
-
-    set --export CLAUDE_CONFIG_DIR "$XDG_DATA_HOME/claude"
-    set --export COMPOSE_BAKE true
-    set --export DOCKER_CONFIG "$XDG_CONFIG_HOME/docker"
-    set --export SSH_AUTH_SOCK "$HOME/.var/app/com.bitwarden.desktop/data/.bitwarden-ssh-agent.sock"
 end

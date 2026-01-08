71de01e30abef4c1b762de4e55d7557fc1e6ea1e
Andrei Zhigalkin <snacks64@proton.me>
2026-01-09 03:38:29 +0500

    Update

--- configuration/fish/config.fish
+++ configuration/fish/config.fish
@@ -1,7 +1,7 @@
 if status is-interactive
     set fish_greeting
 
-    fish_add_path "$HOME/.cargo/bin"
+    fish_add_path "$CARGO_HOME/bin"
+    fish_add_path "$GOPATH/bin/"
     fish_add_path "$HOME/.dotfiles/scripts"
-    fish_add_path "$HOME/.local/bin"
 end

6b73963e1aa9b3a8e97db01d520228f181fab8d9
Andrei Zhigalkin <snacks64@proton.me>
2026-04-07 06:03:58 +0500

    Update

--- configuration/fish/config.fish
+++ configuration/fish/config.fish
@@ -5,4 +5,8 @@ if status is-interactive
     fish_add_path "$GOPATH/bin/"
     fish_add_path "$HOME/.dotfiles/scripts"
     fish_add_path "$HOME/.local/bin"
+
+    if test -x /home/linuxbrew/.linuxbrew/bin/brew
+        eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv fish)"
+    end
 end
